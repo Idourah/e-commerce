ff6ced0026c35f47f6408c0b1d5aa65931afd8cb
k4r4tsub4 <idourah96@gmail.com>
2021-11-07 04:13:20 +0200

    permission errorno13 solved

--- bootshop/bootshop/bootshop/settings.py
+++ bootshop/bootshop/bootshop/settings.py
@@ -143,12 +143,13 @@ META_KEYWORDS = ''
 META_DESCRIPTION = 'BEAUTY CURE is an online supplier of ointments,'
 
 
-STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/images')
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 
